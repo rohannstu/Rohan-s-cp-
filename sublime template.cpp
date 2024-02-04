df8e65f3b0632b96bc76f880e81789fa928a9796
Rohan Sinha <105815725+rohannstu@users.noreply.github.com>
2024-02-04 13:46:17 +0600

    Create sublime template.cpp

---
+++ sublime template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+ ios::sync_with_stdio(0);   
+ cin.tie(0);   
+ #ifndef ONLINE_JUDGE   
+ freopen("input.txt", "r", stdin);   
+ freopen("output.txt", "w", stdout);   
+ #endif 
+
+ cout << "Hello"; 
+}
